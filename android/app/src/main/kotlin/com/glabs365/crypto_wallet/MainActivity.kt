e6512f6d454f48c7af22819232db81a50d1f619f
Gayan <gayan@examgenius.net>
2021-03-06 10:40:07 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/glabs365/crypto_wallet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.glabs365.crypto_wallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home_view.dart
@@ -0,0 +1,119 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypto_wallet/net/api_methods.dart';
+import 'package:crypto_wallet/net/flutterFire.dart';
+import 'package:crypto_wallet/ui/add_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  HomeView({Key key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  double bitcoin = 0.0;
+  double ethereum = 0.0;
+  double tether = 0.0;
+
+  @override
+  initState() {
+    updateValues();
+  }
+
+  updateValues() async {
+    bitcoin = await getPrice("bitcoin");
+    ethereum = await getPrice("ethereum");
+    tether = await getPrice("tether");
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getValue(String id, double amount) {
+      if (id == "bitcoin") {
+        return (bitcoin * amount).toStringAsFixed(2);
+      } else if (id == "ethereum") {
+        return (ethereum * amount).toStringAsFixed(2);
+      } else {
+        return (tether * amount).toStringAsFixed(2);
+      }
+    }
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Users')
+                  .doc(FirebaseAuth.instance.currentUser.uid)
+                  .collection('Coins')
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                return ListView(
+                  children: snapshot.data.docs.map((document) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 8.0, left: 15.0, right: 15.0),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height/12,
+                         decoration: BoxDecoration(
+                           borderRadius: BorderRadius.circular(13.0),
+                             color: Colors.cyan.shade900
+                         ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text("Coin: ${document.id}", style: TextStyle(fontSize: 20.0, color: Colors.white),),
+                                Text(
+                                    "Price: ${getValue(document.id, document.data()['Amount'])}", style: TextStyle(fontSize: 20.0, color: Colors.white),),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.delete,
+                                    color: Colors.red,
+                                  ),
+                                  onPressed: () async{
+                                    await removeCoin(document.id);
+                                  },
+                                ),
+                              ],
+                            ),
+                          )),
+                    );
+                  }).toList(),
+                );
+              }),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddView(),
+            ),
+          );
+        },
+        backgroundColor: Colors.blue,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
